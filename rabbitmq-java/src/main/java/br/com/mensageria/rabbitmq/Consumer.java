f913abbe5a41ae833865dcd29cda366f55c5aaed
Paulo Debatin <paulodebatin@gmail.com>
2021-03-31 07:48:07 -0300

    Tratamento para tipo fonout

--- rabbitmq-java/src/main/java/br/com/mensageria/rabbitmq/Consumer.java
+++ rabbitmq-java/src/main/java/br/com/mensageria/rabbitmq/Consumer.java
@@ -16,24 +16,24 @@ import com.rabbitmq.client.DeliverCallback;
 
 public class Consumer {
 	
-	private final static String QUEUE_NAME = "hello";
+	private final static String QUEUE_NAME = "queue-3";
 	
 	public static void main(String[] args) throws IOException, TimeoutException {
 		
 		ConnectionFactory factory = new ConnectionFactory();
 	    factory.setHost("localhost");
-	    Connection connection = factory.newConnection();
-	    Channel channel = connection.createChannel();
+	    try (Connection connection = factory.newConnection();
+	         Channel channel = connection.createChannel()) {
 
-	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-	    
-	    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-	        String message = new String(delivery.getBody(), "UTF-8");
-	        System.out.println(" [x] Received '" + message + "'");
-	    };
-	    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
-	    
+    	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    	    
+    	    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+    	        String message = new String(delivery.getBody(), "UTF-8");
+    	        System.out.println(" [x] Received '" + message + "'");
+    	    };
+    	    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+	    }
 		
 	}
 
--- rabbitmq-java/src/main/java/br/com/mensageria/rabbitmq/Send.java
+++ rabbitmq-java/src/main/java/br/com/mensageria/rabbitmq/Send.java
@@ -15,20 +15,51 @@ import com.rabbitmq.client.ConnectionFactory;
 
 public class Send {
 	
-	private final static String QUEUE_NAME = "hello";
+	private final static String EXCHANGE_NAME = "msg-exchange";
+	private final static String QUEUE_NAME = "queue-1";
 	
 	public static void main(String[] args) throws IOException, TimeoutException {
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-		
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		String message = "Hello World!";
-		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-		System.out.println(" [x] Sent '" + message + "'");
-		
-		connection.close();
+		//envioParaExchangeTipoDirect();
+	    envioParaExchangeTipoFonout();
+	    
+	    /* Temos um 3 tipo ainda que é o Topic
+	     * Neste caso, mando a mensagem com uma key para uma Exchange. Todas as filas desta Exchange que atenderem a key receberão a msg.
+	     * Alguns exemplos que podem ser utilizados na amarração da queue com a exchange:
+	     *     - routing.#
+	     *     - routing.*
+	     *     - *.key.*
+	     * Mais detalhes em https://faun.pub/different-types-of-rabbitmq-exchanges-9fefd740505d
+	     */
 	}
+	
+	
+	// Manda para um Exchange e todas as queues/filas com binding para este exchange recebem
+	private static void envioParaExchangeTipoFonout() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+               Channel channel = connection.createChannel()) {
+               channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+               String message = "Hello World 2!";
+
+               channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+               System.out.println(" [x] Sent '" + message + "'");
+           }
+    }
+
+	// Manda direto para uma queue/fila
+    private static void envioParaExchangeTipoDirect() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		try (Connection connection = factory.newConnection();
+    		Channel channel = connection.createChannel()) {
+    		
+    		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    		String message = "Hello World!";
+    		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+    		System.out.println(" [x] Sent '" + message + "'");
+		}
+    }
 
 }
